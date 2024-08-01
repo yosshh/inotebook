70020aea84c0b38d78498babcc6b770b767a64ef
yash <yshukla2775@gmail.com>
2024-08-01 23:17:28 +0530

    made some changes

--- backend/index.js
+++ backend/index.js
@@ -12,13 +12,6 @@ const port = process.env.PORT || 5000;
 
 app.use(cors())
 
-app.use(cors(
-  {
-    origin: ["https://deploy-mern-1whq.vercel.app"],
-    methods: ["POST", "GET"],
-    credentials: true
-  }
-))
 app.use(express.json()) // if we want to use json file we will have to put this
 
 
